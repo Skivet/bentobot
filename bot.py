42ec49cef5b0c3c0cb3c3c23a6c3edc6e5bf9ac0
Skivet <spenceredgel@gmail.com>
2023-03-14 11:25:57 -0600

    skeleton

--- bot.py
+++ bot.py
@@ -1,18 +1,73 @@
-import discord, os
+import discord, os, json, requests, random
+import commands, log
 
-# auth
-CLIENT_TOKEN = os.environ['CLIENT_TOKEN']
+# # load cfg
+# with open('config.json') as cfg_file:
+#     cfg = json.load(cfg_file)
+
+#CLIENT_TOKEN = cfg['token']
+
+def reddit_link(subreddit):
+    resp = requests.get(f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=100', headers = {'User-agent': 'BentoBot 0.1'}).json()
+
+    index = random.randrange(1, 100)
+
+    link = resp['data']['children'][index]['data']['url']
+
+    return link
+
 
 class BentoClient(discord.Client):
     async def on_ready(self):
         print('Logged on as: ', self.user)
+        await client.change_presence(activity = discord.Game(name = '!help'))
 
     async def on_message(self, message):
         
+        # verify message is not from self
         if message.author == self.user:
             return
+
+        # check for command prefix
+        if message.content[:1] == '!':
+            print('command detected - ' + message.content)
+
+            args = message.content.split()
+
+            command = args[0][1:]
+
+            if command == 'hentai':
+
+                link = reddit_link('hentai')
+
+                await message.channel.send(link)
+
+            elif command == 'animeme':
+
+                link = reddit_link('animemes')
+
+                await message.channel.send(link)
+
+            elif command == 'meirl':
+
+                link = reddit_link('anime_irl')
+
+                await message.channel.send(link)
             
+            elif command == 'addquote':
+                db.add_quote(message)
+                await message.channel.send('quote saved')
 
-client = BentoClient()
+            elif command == 'quote':
+                quote, author, datetime = db.get_quote()
+                embed = discord.Embed(description=quote, color=0x10FFFF)
+                embed.add_field(name='author', value=f'<@{author}>')
+                embed.add_field(name='date', value=datetime[:10])
+                await message.channel.send(embed=embed)
 
-client.run(CLIENT_TOKEN)
+            else:
+                await message.channel.send('invalid command')            
+
+client = BentoClient(intents=discord.Intents.all())
+
+client.run(os.environ['CLIENT_TOKEN'])
